f0a3f1bbd5d93964652b42fe1e7a32cba967fd31
fleishmang <fleishmang@janelia.hhmi.org>
2020-08-25 16:17:53 -0400

    rearranged for setuptools

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="greedypy",
+    version="0.0.1",
+    author="Greg M. Fleishman",
+    author_email="greg.nli10me@gmail.com",
+    description="Fast deformable registration in python",
+    url="https://github.com/GFleishman/greedypy",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'argparse'
+        'numpy',
+        'scipy',
+        'nibabel',
+        'nrrd',
+        'pyfftw'
+    ]
+)
